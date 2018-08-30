469fd9a7dd28a42638fb5eec80d353846e76014a
RealRedChief <37128377+RealRedChief@users.noreply.github.com>
2018-08-30 18:19:25 -0500

    Set chat to website url

--- public/homepage/js/cs.js
+++ public/homepage/js/cs.js
@@ -91,7 +91,7 @@ function addVideo(theName, vidNumb) {
   const namediv = document.querySelector(`.${theName} img`);
   namediv.addEventListener('click', () => {
     video.src = `https://www.youtube.com/embed/${vidNumb}`;
-    chat.src = `https://www.youtube.com/live_chat?v=${vidNumb}&embed_domain=localhost`;
+    chat.src = `https://www.youtube.com/live_chat?v=${vidNumb}&embed_domain=46.101.192.206`;
   })
 }
 
