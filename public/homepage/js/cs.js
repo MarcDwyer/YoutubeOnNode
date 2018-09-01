628b32758c46340e8ab02562ce6708a83f39f27c
RealRedChief <37128377+RealRedChief@users.noreply.github.com>
2018-09-01 11:53:53 -0500

    Media query now set to 850px

--- public/homepage/js/cs.js
+++ public/homepage/js/cs.js
@@ -91,7 +91,7 @@ const chat = document.querySelector('.chat');
 function addVideo(theName, vidNumb) {
   const namediv = document.querySelector(`.${theName} img`);
   namediv.addEventListener('click', () => {
-    if(window.innerWidth < 751) return;
+    if(window.innerWidth < 850) return;
     video.src = `https://www.youtube.com/embed/${vidNumb}`;
     chat.src = `https://www.youtube.com/live_chat?v=${vidNumb}&embed_domain=www.fetcherapp.net`;
   })
