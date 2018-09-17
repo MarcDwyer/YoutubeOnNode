477f29ffc910905def0a8becc58ea8c61862f06b
Marc Dwyer <envdia@gmail.com>
2018-09-16 21:34:36 -0500

    Inteveral timer decreased

--- public/homepage/js/cs.js
+++ public/homepage/js/cs.js
@@ -74,7 +74,7 @@ fetch (`../fetches/${this.name}stats.json`)
     namediv.addEventListener('click', () => {
       const updateCount = setInterval(() => {
         document.querySelector('.viewcount').textContent = `${this.viewerCount} Viewers`
-      }, 30000)
+      }, 15000)
       document.body.style.backgroundColor = 'black';
       document.querySelector('.viewcount').textContent = `${this.viewerCount} Viewers`;
       video.src = `https://www.youtube.com/embed/${this.vidid}`;
